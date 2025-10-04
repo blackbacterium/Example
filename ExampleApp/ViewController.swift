6f8b5fa9b7ed4cccb7b33f8b2d74335adb2f9933
Black Bacterium <blackbacterium@Blacks-MacBook-Air.local>
2025-10-04 20:37:52 +0300

    Убрал лишние отступы

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Black Bacterium on 04.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
