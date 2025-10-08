110e148623a03962fefed19d597af6c0e570a895
Roman <pimkin-roma@mail.ru>
2025-10-08 19:50:59 +0300

    Update helper and it' usage in the ViewController.

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let helper = Helper() // 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("View did load")
+        updateNumbers()
+    }
+    private func updateNumbers() {
+        helper.addNumber(Int.random(in: 1...10))
+        
+        for number in helper.getNumbers() {
+            print(number)
+        }
     }
 }
 
