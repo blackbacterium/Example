a229e48d41dd2ff16da529bc1ce542779d290661
Roman <pimkin-roma@mail.ru>
2025-10-18 17:46:32 +0300

    Добавил картинку и сделал градиент

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     private let helper = Helper()
     private let textLabel = UILabel()
+    private let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +19,10 @@ class ViewController: UIViewController {
         updateNumbers()
         
         setupLabel()
+        setupImageView()
+        setupView()
         view.addSubview(textLabel)
+        view.addSubview(imageView)
     }
     
     private func updateNumbers() {
@@ -34,6 +38,25 @@ class ViewController: UIViewController {
         textLabel.font = .systemFont(ofSize: 30, weight: .bold)
         textLabel.textColor = .red
         textLabel.frame = CGRect(x: 30, y: 30, width: 100, height: 50)
+    }
+    
+    private func setupImageView() {
+        imageView.image = UIImage(named: "Лиса")
+        imageView.frame = CGRect(x: 30, y: 130, width: 100, height: 200)
+        imageView.layer.shadowColor = UIColor.black.cgColor
+        imageView.layer.shadowOffset = CGSize(width: 15, height: 15)
+        imageView.layer.shadowOpacity = 1
+        imageView.layer.shadowRadius = 10
+    }
+    
+    private func setupView() {
+        let gradient = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.green.cgColor, UIColor.blue.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 1)
+        
+        view.layer.insertSublayer(gradient, at: 0)
     }
 }
 
